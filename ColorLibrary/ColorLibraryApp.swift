51f9fee770b3b0c04b533d8ec6a61541a63dc8ea
NAERNON <naernon@icloud.com>
2023-04-20 15:06:28 +0200

    Initial Commit

---
+++ ColorLibrary/ColorLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorLibraryApp.swift
+//  ColorLibrary
+//
+//  Created by Vivien BERNARD on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
