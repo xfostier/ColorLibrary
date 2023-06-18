779811a8b9eb7d8e51bdf5a64c8c5e91a7e43bad
Xavier Fostier <xavier.fostier@mailfence.com>
2023-06-18 19:28:37 +0200

    JSON is a real bro (way faster)

--- ColorLibrary/Model/ColorDataBase.swift
+++ ColorLibrary/Model/ColorDataBase.swift
@@ -15,41 +15,20 @@ final class ColorDataBase: ObservableObject {
     init(colors: [ColorInfo]) {
         self.colors = colors
         self.colorCache = colors
-        let defaults = UserDefaults.standard
-        do {
-            let datas = defaults.data(forKey: userDefaultKey)
-            guard let datas else { return }
-            // Erase default array if datas in UserDefaults
-            self.colors = try JSONDecoder().decode([ColorInfo].self, from: datas)
-            self.colorCache = self.colors
-        } catch {
-            print("Nothing happen")
-        }
+        // Will erase default colors if some exist on disk
+        readFromJSON()
     }
     
     func append(_ color: ColorInfo) {
         colors.append(color)
-        do {
-            let datas = try JSONEncoder().encode(colors)
-            let defaults = UserDefaults.standard
-            
-            defaults.set(datas, forKey: userDefaultKey)
-        } catch {
-            print("Nothing happened")
-        }
+        colorCache = colors
+        saveToJSON()
     }
     
     func delete(_ color: ColorInfo) {
         colors.removeAll(where: { color == $0 })
         colorCache = colors
-        do {
-            let datas = try JSONEncoder().encode(colors)
-            let defaults = UserDefaults.standard
-            
-            defaults.set(datas, forKey: userDefaultKey)
-        } catch {
-            print("Nothing happened")
-        }
+        saveToJSON()
     }
     
     func filter(searchText: String) {
@@ -74,5 +53,33 @@ final class ColorDataBase: ObservableObject {
     
     static var demo: ColorDataBase {
         .init(colors: [.blue, .cantaloupe, .carnation, .eggPlant, .green, .lemon, .orchid, .red, .salmon, .seaFoam])
+    }
+    
+    func saveToJSON() {
+        do {
+            let file = try FileManager.default
+                .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+                .appendingPathComponent("color_library.json")
+            
+            try JSONEncoder()
+                .encode(colors)
+                .write(to: file)
+        } catch {
+            print("Failure when saving datas")
+        }
+    }
+    
+    func readFromJSON() {
+        do {
+            let file = try FileManager.default
+                .url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+                .appendingPathComponent("color_library.json")
+            
+            let datas = try Data(contentsOf: file)
+            colors = try JSONDecoder().decode([ColorInfo].self, from: datas)
+            colorCache = colors
+        } catch {
+            print("Failure when reading datas")
+        }
     }
 }
